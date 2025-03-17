979075f1d0ce079aeae9a6b2e332ae73597967ed
Minjung Kim <fgh2018@naver.com>
2025-03-17 17:49:05 +0900

    자주 쓰는 미들웨어 추가

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
 
-
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
@@ -10,4 +31,4 @@ app.get('/', (req, res) => {
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
-});
+}); 
